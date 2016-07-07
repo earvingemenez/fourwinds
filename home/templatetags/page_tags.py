14995dab7a56ba40d828c2f91f5340d90e37f5d4
amielsinue <amielsinue@gmail.com>
2016-07-06 23:35:20 -0500

    - organizing templates in order to optimize creation of other pages

---
+++ home/templatetags/page_tags.py
@@ -0,0 +1,38 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('top_header.html', takes_context=True)
+def render_topheader(context):
+    return {}
+
+
+@register.inclusion_tag('top_navbar.html', takes_context=True)
+def render_topnavbar(context):
+    return {}
+
+
+@register.inclusion_tag('widgets.html', takes_context=True)
+def render_widgets(context):
+    return {}
+
+
+@register.inclusion_tag('get_started_form.html', takes_context=True)
+def render_get_started_form(context):
+    return {}
+
+
+@register.inclusion_tag('welcome_signin_form.html', takes_context=True)
+def render_welcome_signin_form(context):
+    return {}
+
+
+@register.inclusion_tag('testimonials.html', takes_context=True)
+def render_testimonials(context):
+    return {}
+
+
+@register.inclusion_tag('footer.html', takes_context=True)
+def render_footer(context):
+    return {}
