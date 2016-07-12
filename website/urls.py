81471dbff3d49fbe696759e4bcca0d9741d291b5
amielsinue <amielsinue@gmail.com>
2016-07-11 20:10:32 -0500

    - adding travel page

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^safety-and-insurance/$', views.safety_insurance, name='safety_insurance'),
     url(r'^request-a-quote/$', views.request_quote, name='request_quote'),
+    url(r'^travel/$', views.travel, name='travel'),
 ]
--- website/views.py
+++ website/views.py
@@ -5,8 +5,13 @@ def index(request):
     return render(request, 'website/index.html')
 
 
+def travel(request):
+    return render(request, 'website/travel.html')
+
+
 def safety_insurance(request):
     return render(request, 'website/safety_insurance.html')
+
 
 def request_quote(request):
     return render(request, 'website/request_quote.html')
