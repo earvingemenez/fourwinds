912e366baf00fb4bf9d823389e42e2a789cd35e8
amielsinue <amielsinue@gmail.com>
2016-07-14 00:33:17 -0500

    - adding Events Admin models

--- fourwinds/settings.py
+++ fourwinds/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os, dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media_cdn')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
--- website/models.py
+++ website/models.py
@@ -1,5 +1,73 @@
 from __future__ import unicode_literals
 
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch.dispatcher import receiver
+
+
+def get_path(instance, filename, path):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join(path, filename)
+
+
+def get_event_photos_path(instance, filename):
+    return get_path(instance, filename, 'events')
+
+
+def get_quotes_files_path(instance, filename):
+    return get_path(instance, filename, 'quotes')
+
+
+class EventCategory(models.Model):
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    title = models.CharField(max_length=100)
+    caption = models.CharField(max_length=200)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Event(models.Model):
+
+    class Meta:
+        verbose_name = "Event"
+        verbose_name_plural = "Events"
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    category = models.ForeignKey(EventCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class EventPhotos(models.Model):
+
+    class Meta:
+        verbose_name = "Photo"
+        verbose_name_plural = "Photos"
+
+    photo = models.ImageField(upload_to=get_event_photos_path,
+                              null=False,
+                              blank=False)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+
+@receiver(post_delete, sender=EventPhotos)
+def event_photo_delete(sender, instance, **kwargs):
+    instance.photo.delete(False)
+
+
