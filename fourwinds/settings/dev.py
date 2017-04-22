30494143885ca8c46b37dc5c8d970ae3ed885ef0
amielsinue <amielsinue@gmail.com>
2017-04-22 18:51:30 -0500

    - refactor fourwinds to use wagtail-cms framework - initial commit

---
+++ fourwinds/settings/dev.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '20g&ri1i7pf$e*_kml%1rrlo%yvy+mn2n57djw2r8%x6&akxv#'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
--- fourwinds/wsgi.py
+++ fourwinds/wsgi.py
@@ -4,17 +4,15 @@ WSGI config for fourwinds project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
+
+from __future__ import absolute_import, unicode_literals
 
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fourwinds.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fourwinds.settings.dev")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-application.add_files(os.path.join(os.path.dirname(BASE_DIR), 'media'), prefix='media/')
