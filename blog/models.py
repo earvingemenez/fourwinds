976c9129c57fc89a23862dcb1707732a18f5984f
amielsinue <amielsinue@gmail.com>
2017-04-29 14:16:48 -0500

    - remove blog categories - add missing migrations in prev commit

--- blog/models.py
+++ blog/models.py
@@ -89,23 +89,3 @@ class BlogTagIndexPage(Page):
         context = super(BlogTagIndexPage, self).get_context(request)
         context['blogpages'] = blogpages
         return context
-
-
-@register_snippet
-class BlogCategory(models.Model):
-    name = models.CharField(max_length=255)
-    icon = models.ForeignKey(
-        'wagtailimages.Image', null=True, blank=True,
-        on_delete=models.SET_NULL, related_name='+'
-    )
-
-    panels = [
-        FieldPanel('name'),
-        ImageChooserPanel('icon'),
-    ]
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = 'blog categories'
