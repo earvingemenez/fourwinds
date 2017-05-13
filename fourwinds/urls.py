4b47cef0a670ce9aa5f327211fb58acc20996800
amielsinue <amielsinue@gmail.com>
2017-05-13 13:41:39 -0500

    - install blog comments

--- fourwinds/urls.py
+++ fourwinds/urls.py
@@ -17,15 +17,20 @@ urlpatterns = [
 
     url(r'^search/$', search_views.search, name='search'),
 
+    url(r'^blog/', include('blog.urls', namespace="blog")),
+    url(r'^comments/', include('django_comments_xtd.urls')),
+
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
     url(r'', include(wagtail_urls)),
 
+
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    url(r'^pages/', include(wagtail_urls)),
 ]
+
 
 
 if settings.DEBUG:
