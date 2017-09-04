ba3525abb59a48c1c26c2d9326cb82adc68e2ccd
Earvin Gemenez <earvin.gemenez@gmail.com>
2017-09-04 12:33:28 +0800

    Fix email backend

--- fourwinds/settings/dev.py
+++ fourwinds/settings/dev.py
@@ -3,17 +3,15 @@ from __future__ import absolute_import, unicode_literals
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '20g&ri1i7pf$e*_kml%1rrlo%yvy+mn2n57djw2r8%x6&akxv#'
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 BASE_URL = 'http://localhost:8000/'
 MEDIA_URL = '/media/'
 STATIC_URL = '/assets/'
-print "Static url: {}".format(STATIC_URL)
+
 try:
     from .local import *
 except ImportError:
